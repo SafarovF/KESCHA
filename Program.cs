0994723eaadedfc96eaf00f2d5312bfcd173efa4
SafarovF <133909612+SafarovF@users.noreply.github.com>
2023-07-19 04:55:21 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+string greeting = $"Hello {name}";
+System.Console.WriteLine(greeting);
+
+System.Console.Write("Enter you age: ");
+string ageAsString = Console.ReadLine();
+System.Console.WriteLine("Converting...");
+int age = Convert.ToInt32(ageAsString);
+System.Console.WriteLine($"Successfully converted ! {age}");
+int keschasAge = 3;
+int ageDifference = age - keschasAge;
+System.Console.WriteLine($"The difference between your and Kescha's age is {ageDifference}");
